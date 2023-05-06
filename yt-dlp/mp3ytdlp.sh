e7c8474f77d74f48a6e45121bc7669975b9ee34a
barkwoofdog <97663379+barkwoofdog@users.noreply.github.com>
2023-05-06 02:16:45 -0500

    initial mp3 script

--- yt-dlp/mp3ytdlp.sh
+++ yt-dlp/mp3ytdlp.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+echo paste link to video to extract audio from
+
+read vidLink
+
+echo type desired filename. extension is already MP3
+
+read filename
+
+yt-dlp -f 'ba' -x --audio-format mp3 $vidLink -o $HOME/$filename.mp3
+
+echo $filename.mp3 download is complete
